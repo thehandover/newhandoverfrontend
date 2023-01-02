7a54300d09f10808af2977f314ff34b3f2729d96
usama-ejaz-digitify <usama.ejaz@digitify.com>
2023-01-03 04:22:29 +0500

    fix

--- config/constants.tsx
+++ config/constants.tsx
@@ -1,5 +1,5 @@
 const API_LINK = process.env.API_URL;
-const Base_URL = process.env.NODE_ENV=='development' ? 'http://127.0.0.1:3000' : 'https://thehandover.com';
+const Base_URL = 'https://thehandover.com';
 const Stripe_Keys = {
     'production': {
         Secret_Key : process.env.STRIPE_LIVE_SECRET,
